9ee71c3204b2df21edbced9a05229e9e6046ec60
vikk <vikkivillalba30@gmail.com>
2025-07-02 16:11:25 -0700

    Proyecto
    Interfaz gráfica de navegación

---
+++ grafoPuebla/src/main/java/com/mycompany/grafopuebla/GrafoPuebla.java
@@ -0,0 +1,15 @@
+package com.mycompany.grafopuebla;
+
+import frames.FrmPrincipal;
+
+/**
+ *
+ * @author victoria
+ */
+public class GrafoPuebla {
+
+    public static void main(String[] args) {
+        FrmPrincipal pantalla = new FrmPrincipal();
+        pantalla.setVisible(true);
+    }
+}
